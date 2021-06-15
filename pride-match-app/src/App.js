86b1689855a5ccdb6be4bf7923646910e6277ebc
JP-sDEV <jon.pablo80@gmail.com>
2021-06-15 14:14:33 -0400

    added google auth

--- pride-match-app/src/App.js
+++ pride-match-app/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavigationBar from "./components/NavigationBar";
+import socialMediaAuth from './service/auth';
+import { googleProvider } from './firebase_config/authMethod';
 
 function App() {
+
+    const handleOnClick = async (provider) => {
+        const res = await socialMediaAuth(provider);
+        console.log(res);
+    }
+
     return (
         <div className="App">
             <NavigationBar/>
             {/*<h1>Home</h1>*/}
+
+            <button onClick={() => handleOnClick(googleProvider)}>Google Login</button>
+
         </div>
     );
 }
