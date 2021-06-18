d4cb2da2c17fe7d5ccb7b405512b316aa90d3892
Yukie Man <yukie.man97@gmail.com>
2021-06-18 08:54:53 -0700

    made emptylist check for forums

--- pride-match-app/src/components/forums/Forum.js
+++ pride-match-app/src/components/forums/Forum.js
@@ -1,13 +1,14 @@
 import { Component } from "react";
 import { Container } from "react-bootstrap";
 import "./Forum.css"
+import "../../App.css"
 import { renderForumPost, renderForumLink} from "../../helpers";
 
 export class Forum extends Component {
     constructor(props) {
         super(props)
-        this.state = { allGamePosts:[ "game1", "game2", "game3", "game4", "game4", "game5", "game6", "game7"],
-            allGeneralPosts: ["general1", "general2", "general3", "general4", "general5", "general6", "general7"]}
+        this.state = { allGamePosts:[ "game1", "game2", "game3", "game4", "game4", "game5", "game6", "game7" ],
+            allGeneralPosts: [ "general1", "general2", "general3", "general4", "general5", "general6", "general7" ]}
     }
 
     renderSwitch(type) {
@@ -26,14 +27,18 @@ export class Forum extends Component {
                 console.log(newestGamePosts)
                 return <div>
                     {renderForumPost(newestGamePosts, type)}
-                    {renderForumLink("/forums/games", "See All Posts", "link-colour")}
+                    {this.state.allGamePosts.length === 0
+                        ? ""
+                        : renderForumLink("/forums/games", "See All Posts", "link-colour")}
                 </div>
             case 'general':
                 let newestGeneralPosts = this.state.allGeneralPosts.slice(0, displayNumPosts)
                 console.log(newestGeneralPosts)
                 return <div>
                     {renderForumPost(newestGeneralPosts, type)}
-                    {renderForumLink("/forums/games", "See All Posts", "link-colour")}
+                    {this.state.allGeneralPosts.length === 0
+                        ? ""
+                        : renderForumLink("/forums/games", "See All Posts", "link-colour")}
                 </div>
             default:
                 break
@@ -44,6 +49,10 @@ export class Forum extends Component {
         return (
             <Container className="forum-layout">
                 <h4 className="forum-title">Currently Posted...</h4>
+                {this.state.allGamePosts.length === 0
+                ? <span className="no-display">No Game Posts to Display</span>  : ""}
+                {this.state.allGeneralPosts.length === 0
+                    ? <span className="no-display">No General Posts to Display</span>  : ""}
                 {this.renderSwitch(this.props.type)}
             </Container>
         )
