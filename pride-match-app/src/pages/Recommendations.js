b437dfcd87304445722b4a98cbbcbb561a5e9ef5
Yukie Man <yukie.man97@gmail.com>
2021-06-17 22:26:47 -0700

    add view all teammates button to rec page

--- pride-match-app/src/pages/Recommendations.js
+++ pride-match-app/src/pages/Recommendations.js
@@ -1,11 +1,9 @@
 import {Component} from "react";
 import NavigationBar from "../components/NavigationBar";
-import IndividualTeammate from "../components/landing/IndividualTeammate";
-import {Image, Row} from "react-bootstrap";
+import {Image, Row, Button} from "react-bootstrap";
 import teammate from "../assets/teammate.png";
 import recommendations from "../assets/recommendations.png";
 import "../components/recommendations/recommendations.css"
-import TeammatesTabs from "../components/recommendations/TeammatesTabs";
 import DisplayTeammates from "../components/landing/DisplayTeammates";
 export class Recommendations extends Component {
     render() {
@@ -14,7 +12,8 @@ export class Recommendations extends Component {
                 <NavigationBar/>
                 <Image src={teammate} alt="Teammate"/>
                 <Image className="level-off" src={recommendations} alt="Recommendations"/>
-                <TeammatesTabs />
+                <br/>
+                <Button className="colourful-button" variant="link">View All My Teammates</Button>
                 <Row className="game-container">
                     {/*TODO make recommendations dynamic*/}
                     {/*note: recommended teammates will come from recommended API call*/}
