88122132a8a5f6771a37732256f42c70ea066d98
Yukie Man <yukie.man97@gmail.com>
2021-06-18 08:07:00 -0700

    done render general forums

--- pride-match-app/src/components/forums/Forum.js
+++ pride-match-app/src/components/forums/Forum.js
@@ -7,8 +7,7 @@ export class Forum extends Component {
     constructor(props) {
         super(props)
         this.state = { allGamePosts:[ "game1", "game2", "game3", "game4", "game4", "game5", "game6", "game7"],
-            allGeneralPosts: ["general1", "general2", "general3", "general4"],
-            someGeneralPosts: ["general1", "general2", "general3"]}
+            allGeneralPosts: ["general1", "general2", "general3", "general4", "general5", "general6", "general7"]}
     }
 
     renderSwitch(type) {
@@ -18,16 +17,22 @@ export class Forum extends Component {
                 return <div>
                     {renderForumPost(this.state.allGamePosts, type)}
                 </div>
-            case 'general':
+            case 'allGeneral':
                 return <div>
-                    {renderForumPost(this.state.someGeneralPosts, type)}
-                    {renderForumLink("/forums/general", "See All Posts", "link-colour")}
+                    {renderForumPost(this.state.allGeneralPosts, type)}
                 </div>
             case 'games':
                 let newestGamePosts = this.state.allGamePosts.slice(0, displayNumPosts)
                 console.log(newestGamePosts)
                 return <div>
                     {renderForumPost(newestGamePosts, type)}
+                    {renderForumLink("/forums/games", "See All Posts", "link-colour")}
+                </div>
+            case 'general':
+                let newestGeneralPosts = this.state.allGeneralPosts.slice(0, displayNumPosts)
+                console.log(newestGeneralPosts)
+                return <div>
+                    {renderForumPost(newestGeneralPosts, type)}
                     {renderForumLink("/forums/games", "See All Posts", "link-colour")}
                 </div>
             default:
