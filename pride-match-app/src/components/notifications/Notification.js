33664f354f48b3c59790e4583cb0d6c19a7904f1
Yukie Man <yukie.man97@gmail.com>
2021-06-18 02:42:50 -0700

    added no notifications to display msg

--- pride-match-app/src/components/notifications/Notification.js
+++ pride-match-app/src/components/notifications/Notification.js
@@ -1,18 +1,23 @@
 import {Component} from "react";
-import {Button, Card, Container, Image} from "react-bootstrap";
 import "./notifications.css"
+import "../landing/Landing.css"
 import NotificationPost from "./NotificationPost";
 
 export class Notification extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {allNotifications: ["notif1", "notif2", "notif3", "notif4", "notif5", "notif6"]}
+    }
+
     render() {
         return (
             <div>
-                <NotificationPost key="1"/>
-                <NotificationPost key="2"/>
-                <NotificationPost key="3"/>
-                <NotificationPost key="4"/>
-                <NotificationPost key="5"/>
-                <NotificationPost key="6"/>
+                {this.state.allNotifications.length === 0
+                    ? <span className="no-display">No Notifications to Display</span>
+                    : this.state.allNotifications.map((notif, index) => {
+                        return <NotificationPost key={index}/>
+                    })
+                }
             </div>
         )
     }
