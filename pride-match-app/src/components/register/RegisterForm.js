12676ef540fd3a22091acbadf6f3315f43986553
JP-sDEV <jon.pablo80@gmail.com>
2021-06-18 16:26:54 -0400

    formatted registration json

--- pride-match-app/src/components/register/RegisterForm.js
+++ pride-match-app/src/components/register/RegisterForm.js
@@ -22,19 +22,21 @@ export class RegisterForm extends Component {
             confirmPassword: "",
             email: "",
             confirmEmail: "",
-            regOrientation: ORIENTATION_LIST[0],
+            orientation: ORIENTATION_LIST[0],
             age: "",
             pronouns: PRONOUNS_LIST[0],
             gender: GENDER_LIST[0],
-            displayPronouns: false,
-            displayOrientation: false,
-            displayGender: false,
-            games: [],
-            interests: [],
-            discord: "",
+            display_pronouns: false,
+            display_orientation: false,
+            display_gender: false,
+            games: "",
+            interests: "",
+            discord_id: "",
             twitter: "",
             instagram: "",
-            facebook: ""
+            facebook: "",
+            about_me: ""
+
         }
         this.listStates = {
             orientationList: ORIENTATION_LIST,
@@ -44,8 +46,9 @@ export class RegisterForm extends Component {
     }
 
     handleSubmit = () => {
+
         let requiredStates = [this.state.username, this.state.password, this.state.confirmPassword, this.state.email,
-            this.state.confirmEmail,this.state.regOrientation, this.state.age, this.state.pronouns, this.state.gender]
+            this.state.confirmEmail,this.state.reg_orientation, this.state.age, this.state.pronouns, this.state.gender]
         if (requiredStates.includes("") || this.state.games.length === 0) {
             NotificationManager.warning("Please fill out all required (*) information!")
         } else if (this.state.email.toLowerCase() !== this.state.confirmEmail.toLowerCase()) {
@@ -53,6 +56,30 @@ export class RegisterForm extends Component {
         } else if (this.state.password.toLowerCase() !== this.state.confirmPassword.toLowerCase()) {
             NotificationManager.warning("Passwords do not match!", "", NOTIFICATION_TIMER)
         } else {
+
+            let registrationFormatted = {
+                "username": this.state.username, 
+                "password": this.state.password,
+                "email": this.state.email,
+                "gender": this.state.gender,
+                "age": this.state.age,
+                "orientation": this.state.reg_orientation,
+                "pronouns": this.state.pronouns,
+                "about_me": this.state.about_me,
+                "display_orientation": this.state.display_orientation,
+                "display_gender": this.state.display_gender,
+                "display_pronouns": this.state.display_pronouns,
+                "socials": 
+                    {
+                        "facebook": this.state.facebook,
+                        "instagram": this.state.instagram,
+                        "twitter": this.state.twitter,
+                        "discord_id": this.state.discord_id
+                    },
+                "interests": this.state.interests.split(','),
+                "games": this.state.games.split(',')
+                
+            }
             // TODO change this.props.state's profilePicture and sessionToken
             this.props.setState({
                 ...this.props.state,
@@ -62,12 +89,10 @@ export class RegisterForm extends Component {
             })
             this.props.setUserState({...this.props.userState, completed: true})
             NotificationManager.success("Welcome to Pride Match, " + this.state.username + "!", "", NOTIFICATION_TIMER)
+            sendRegistrationPost(registrationFormatted)
         }
         SaveStateToLocal(this.props.state)
-        sendRegistrationPost(this.props.state)
-        console.log(sendRegistrationPost(this.props.state))
-
-
+        // console.log(sendRegistrationPost(this.props.state))
     }
 
     handleGoogleAutofill = async (provider) => {
@@ -126,8 +151,8 @@ export class RegisterForm extends Component {
     }
 
     handlePronounsDisplay = (e) => {
-        let prevPronounsDisplay = this.state.displayPronouns
-        this.setState({displayGender: !prevPronounsDisplay})
+        let prevPronounsDisplay = this.state.displayy_pronouns
+        this.setState({display_gender: !prevPronounsDisplay})
     }
 
     setGender = (e) => {
@@ -135,8 +160,8 @@ export class RegisterForm extends Component {
     }
 
     handleGenderDisplay = () => {
-        let prevGenderDisplay = this.state.displayGender
-        this.setState({displayGender: !prevGenderDisplay})
+        let prevGenderDisplay = this.state.display_gender
+        this.setState({display_gender: !prevGenderDisplay})
     }
 
     setOrientation = (e) => {
@@ -144,8 +169,8 @@ export class RegisterForm extends Component {
     }
 
     handleOrientationDisplay = () => {
-        let prevOrientationDisplay = this.state.displayOrientation
-        this.setState({displayOrientation: !prevOrientationDisplay})
+        let prevOrientationDisplay = this.state.display_orientation
+        this.setState({display_orientation: !prevOrientationDisplay})
     }
 
     setGames = (e) => {
@@ -157,7 +182,7 @@ export class RegisterForm extends Component {
     }
 
     setDiscord = (e) => {
-        this.setState({discord: e.target.value})
+        this.setState({discord_id: e.target.value})
     }
 
     setTwitter = (e) => {
@@ -172,6 +197,9 @@ export class RegisterForm extends Component {
         this.setState({facebook: e.target.value})
     }
 
+    setAboutMe = (e) => {
+        this.setState({about_me: e.target.value})
+    }
     render() {
         return (
             <div>
@@ -349,7 +377,7 @@ export class RegisterForm extends Component {
                                     <Form.Control
                                         rows={2}
                                         as="textarea"
-                                        onChange={this.setInterests}
+                                        onChange={this.setAboutMe}
                                         placeholder="Add anything extra!"/>
                                     <Form.Text muted>Max 255 Characters</Form.Text>
                                 </Form.Group>
