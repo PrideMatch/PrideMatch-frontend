0dea37250717c63959378696bf1ae51963cfba05
Yukie Man <yukie.man97@gmail.com>
2021-06-18 17:17:08 -0700

    added forums data

---
+++ pride-match-app/src/sampleGameForumData.js
@@ -0,0 +1,33 @@
+export const gameForums = [
+    {
+        "title": "E3 News",
+        "author": "Shaun",
+        "description": "2021 kicked off with news about E3 2022. Kind of a funny way to start a show, as Mayor Eric " +
+            "Garcetti told the crowd, “we look forward to seeing you in-person, here in the City of Angels, in 2022.” " +
+            "Also a bit funny when the mayor’s video game show announcement has less confetti and Minions than his " +
+            "state-reopening speech, but that’s something for another post."
+    },
+    {
+        "title": "PS5 Restock For GameStop PowerUp Rewards Pro Members At 11:30 AM ET Today",
+        "author": "Yukie",
+        "description": "Update: GameStop has notified PowerUp Rewards Pro members that they will be able to purchase " +
+            "a PS5 at 8:30 AM PT / 11:30 AM ET on the GameStop website. If you're a member, check your email for the " +
+            "notification. Be sure you're logged in on the GameStop website ahead of time to take advantage of the " +
+            "early access sale."
+    },
+    {
+        "title": "Mortal Kombat Star Hiroyuki Sanada Joins Cast of John Wick Chapter 4",
+        "author": "JP",
+        "description": "The cast for John Wick: Chapter 4 continues to get more and more awesome. First it was " +
+            "Donnie Yen, then Laurence Fishburne. Now, Hiroyuki Sanada--known most recently for his turn as Scorpion " +
+            "in the new Mortal Kombat movie--enters the fray as well, Deadline reports."
+    },
+    {
+        "title": "Elden Ring World-Builder George RR Martin Thinks It's A Dark Souls Sequel",
+        "author": "Michelle",
+        "description": "A Song of Ice and Fire author George R.R. Martin, whose book series was turned into HBO's " +
+            "Game of Thrones, also helped to build the world of Elden Ring and create some of its characters. " +
+            "However, during an interview with Chicago news network WTTW, Martin seems to misrepresent the game " +
+            "as a sequel to Dark Souls. "
+    }
+]
---
+++ pride-match-app/src/sampleGeneralForumData.js
@@ -0,0 +1,42 @@
+export const generalForums = [
+    {
+        "title":"Clippers F Kawhi Leonard listed as out for Friday’s Game 6 vs. Utah",
+        "author": "Grewor",
+        "description": "LOS ANGELES (AP) — Clippers forward Kawhi Leonard is officially out Friday night for Game " +
+            "6 of the Western Conference semifinals against the Utah Jazz, but coach Tyronn Lue did not have any " +
+            "update on if it will be longer.\n" +
+            "\n" +
+            "Leonard sprained his right knee Monday night in a Game 4 win. He traveled with the Clippers to Salt " +
+            "Lake City, Lue said, then returned to Los Angeles for further testing. A sprain, by definition, is a " +
+            "ligament injury, but the Clippers have not said which of the four knee ligaments he injured."
+    },
+    {
+        "title":"A Pizza Recipe With Artichoke, Mozzarella — and Cicadas",
+        "author": "Shaun",
+        "description": "David George Gordon, author of the “Eat-a-Bug Cookbook,” says periodical cicadas should be " +
+            "harvested immediately after they have undergone their final molt — usually within minutes of " +
+            "their appearance above ground."
+    },
+    {
+        "title":"COVID-19: 109 new cases, 1 new death as B.C. heads into 1st weekend of eased restrictions",
+        "author": "Braum",
+        "description": "British Columbia reported 109 new COVID-19 cases and one additional death Friday, as the" +
+            " province heads into its first weekend under loosened health restrictions."
+    },
+    {
+        "title": "Happy International Sushi Day!",
+        "author": "Kiki",
+        "description": "Today is International Sushi Day and a perfect excuse to treat yourself to delicious " +
+            "homemade Sanrio-themed sushi!" +
+            "Hi, everyone! I’m Michael, a San Francisco Bay Area-based food blogger and home chef! My goal is to inspire " +
+            "people to create food that is not only delicious but are beautiful works of art."
+    },
+    {
+        "title":"Vaccine rule for Springsteen Broadway show adds travel uncertainty for AstraZeneca recipients",
+        "author": "Tiffany",
+        "description": "David Screech had already selected his seats and put in his credit card information for " +
+            "Springsteen on Broadway tickets when he noticed the COVID-19 vaccine requirements: his two doses of " +
+            "AstraZeneca-Oxford wouldn't be enough for admission."
+    },
+
+]
